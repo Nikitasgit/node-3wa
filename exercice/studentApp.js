720915cdb8e9e16205717c2dffc7a52adb72b847
Brasero <brandonricci4@gmail.com>
2025-05-12 19:03:08 +0200

    proposition de correction

--- exercice/studentApp.js
+++ exercice/studentApp.js
@@ -1,4 +1,4 @@
-const fs = require('node:fs')
+import fs from 'node:fs';
 
 const commands = [
 	{
@@ -15,8 +15,62 @@ const commands = [
 	}
 ]
 
+const extractArg = (text) => {
+	const elem = text.split(' ')
+	if (elem.length < 3) return elem[1];
+	return elem[1] + ' ' + elem[2]
+}
+
+
+
+const students = JSON.parse(fs.readFileSync('./Data/students.json', {encoding: 'utf8'}))
+
+const list = () => {
+	const names = students.map(student => student.name)
+	console.log(names.join('\n'))
+}
+
+const find = (name) => {
+	const student = students.find((student) => student.name.trim().toLowerCase() === name.trim().toLowerCase())
+	if (!student) {
+		console.log(`L'élève ${name} n'existe pas.`)
+		return
+	}
+	console.table(student)
+}
+
+const more = (num) => {
+	const filterStudent =  students.filter(student => {
+		return (student.notes.reduce((acc, curr) => acc + curr, 0) / student.notes.length) > num
+	})
+	
+	console.table(filterStudent)
+}
 
 process.stdin.on("data", (chunk) => {
 	const data = chunk.toString()
-	const text = data.replace("\r?\n", '')
+	const text = data.replace("\r\n", '')
+	let arg;
+	
+	switch (text) {
+		
+		case 'list':
+			list()
+			return
+		
+		case text.match(/^find /) ? text : null :
+			arg = extractArg(text)
+			find(arg)
+			return
+		
+		case text.match(/^more /) ? text : null:
+			arg = extractArg(text)
+			more(arg)
+			return
+		
+		default:
+			console.group('Commande inconnu, voici la liste des commandes')
+			console.table(commands)
+			console.groupEnd()
+	}
 })
